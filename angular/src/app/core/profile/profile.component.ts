95a85db0b2076d3604e2f88ff1cde294f56f8b7a
Alkinos595 <foinquinosalberto@gmail.com>
2024-07-06 16:07:56 +0200

    userdata section added

--- angular/src/app/core/profile/profile.component.ts
+++ angular/src/app/core/profile/profile.component.ts
@@ -5,7 +5,12 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 	selector: 'app-profile',
 	standalone: true,
 	imports: [RouterOutlet, RouterLink],
-	templateUrl: './profile.component.html'
+	templateUrl: './profile.component.html',
+	styles: [`
+		.login:hover h5 {
+			color: white;
+		}
+	`]
 })
 export class ProfileComponent {
 
--- angular/src/app/shared/userdata/userdata.component.ts
+++ angular/src/app/shared/userdata/userdata.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-userdata',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './userdata.component.html'
 })
 export class UserdataComponent {
